7135df2bf0543e38bfec5b3d32a689c9bb55897a
samuelraymond23 <samuel.raymond@bison.howard.edu>
2018-03-07 03:44:03 -0500

    Search bar works with hardcoded names

--- src/pages/friend/friend.ts
+++ src/pages/friend/friend.ts
@@ -9,31 +9,70 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FriendPage {
 	
-	items = [
-	    'Pokémon Yellow',
-	    'Super Metroid',
-	    'Mega Man X',
-	    'The Legend of Zelda',
-	    'Pac-Man',
-	    'Super Mario World',
-	    'Street Fighter II',
-	    'Half Life',
-	    'Final Fantasy VII',
-	    'Star Fox',
-	    'Tetris',
-	    'Donkey Kong III',
-	    'GoldenEye 007',
-	    'Doom',
-	    'Fallout',
-	    'GTA',
-	    'Halo'
-	  ];
+	 constructor() {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Buenos Aires',
+      'Cairo',
+      'Dhaka',
+      'Edinburgh',
+      'Geneva',
+      'Genoa',
+      'Glasglow',
+      'Hanoi',
+      'Hong Kong',
+      'Islamabad',
+      'Istanbul',
+      'Jakarta',
+      'Kiel',
+      'Kyoto',
+      'Le Havre',
+      'Lebanon',
+      'Lhasa',
+      'Lima',
+      'London',
+      'Los Angeles',
+      'Madrid',
+      'Manila',
+      'New York',
+      'Olympia',
+      'Oslo',
+      'Panama City',
+      'Peking',
+      'Philadelphia',
+      'San Francisco',
+      'Seoul',
+      'Taipeh',
+      'Tel Aviv',
+      'Tokio',
+      'Uelzen',
+      'Washington'
+    ];
+  }
 
 	  itemSelected(item: string) {
 	    console.log("Selected Item", item);
 	  }
 	
-	 
+	 getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
