3b4092fab15bbed59d63f57c96d9bf93068f3882
samuelraymond23 <samuel.raymond@bison.howard.edu>
2018-03-07 03:16:09 -0500

    Implemented the Friends list, Will soon be integrated with firebase

--- src/pages/friend/friend.ts
+++ src/pages/friend/friend.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the FriendPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,9 +8,36 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'friend.html',
 })
 export class FriendPage {
+	
+	items = [
+	    'Pokémon Yellow',
+	    'Super Metroid',
+	    'Mega Man X',
+	    'The Legend of Zelda',
+	    'Pac-Man',
+	    'Super Mario World',
+	    'Street Fighter II',
+	    'Half Life',
+	    'Final Fantasy VII',
+	    'Star Fox',
+	    'Tetris',
+	    'Donkey Kong III',
+	    'GoldenEye 007',
+	    'Doom',
+	    'Fallout',
+	    'GTA',
+	    'Halo'
+	  ];
 
+	  itemSelected(item: string) {
+	    console.log("Selected Item", item);
+	  }
+	
+	 
+  
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FriendPage');
