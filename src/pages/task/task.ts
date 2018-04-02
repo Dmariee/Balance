6a96465447f21fa8cce8418eb3a67c22800b1357
unknown <dmarieehanson@yahoo.com>
2018-04-02 19:27:12 -0400

    Corected month display

--- src/pages/task/task.ts
+++ src/pages/task/task.ts
@@ -14,7 +14,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'task.html',
 })
 export class TaskPage {
-
+    months = ["JANUARY ","FEBRUARY ","MARCH ","APRIL ","MAY ","JUNE ","JULY ","AUGUST ","SEPTEMBER ","OCTOBER ","NOVEMBER ","DECEMBER "];
+    eventsHolder: any = [];
     eventSource: any = [];
     viewTitle;
     isToday: boolean;
@@ -24,70 +25,26 @@ export class TaskPage {
     };
 
     addEvent() {
-        this.eventSource = this.createRandomEvents();
-    }
-
-    onViewTitleChanged(title) {
-        this.viewTitle = title;
-        console.log(this.viewTitle);
-    }
-
-    onEventSelected(event) {
-        console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
-        // Later add navigation to discussion board.
-    }
-
-    changeMode(mode) {
-        this.calendar.mode = mode;
+        var date = new Date();
+        var startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 2, 0, date.getMinutes() + 12);
+        var endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 2, 0, date.getMinutes() + 19);
+        this.eventsHolder.push(
+        {
+            title: "Tester",
+            startTime:  startTime,
+            endTime:  endTime,
+            allDay: false
+        });
+        this.eventSource = this.eventsHolder;
     }
 
     today() {
         this.calendar.currentDate = new Date();
     }
 
-    onCurrentDateChanged(event:Date) {
-        var today = new Date();
-        today.setHours(0, 0, 0, 0);
-        event.setHours(0, 0, 0, 0);
-        this.isToday = today.getTime() === event.getTime();
-    }
-
-    createRandomEvents() {
-        var events = [];
-        for (var i = 0; i < 50; i += 1) {
-            var date = new Date();
-            var eventType = Math.floor(Math.random() * 2);
-            var startDay = Math.floor(Math.random() * 90) - 45;
-            var endDay = Math.floor(Math.random() * 2) + startDay;
-            var startTime;
-            var endTime;
-            if (eventType === 0) {
-                startTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + startDay));
-                if (endDay === startDay) {
-                    endDay += 1;
-                }
-                endTime = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + endDay));
-                events.push({
-                    title: 'All Day - ' + i,
-                    startTime: startTime,
-                    endTime: endTime,
-                    allDay: true
-                });
-            } else {
-                var startMinute = Math.floor(Math.random() * 24 * 60);
-                var endMinute = Math.floor(Math.random() * 180) + startMinute;
-                startTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + startDay, 0, date.getMinutes() + startMinute);
-                endTime = new Date(date.getFullYear(), date.getMonth(), date.getDate() + endDay, 0, date.getMinutes() + endMinute);
-                events.push({
-                    title: 'Event - ' + i,
-                    startTime: startTime,
-                    endTime: endTime,
-                    allDay: false
-                });
-            }
-        }
-        return events;
-    }
+    onCurrentDateChanged = (ev: Date) => {
+        this.viewTitle = this.months[ev.getMonth()] + ev.getFullYear();
+    };
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
